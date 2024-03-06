561dfb7e6e886b80cf292b751068f3b9fb1cc8c3
Rodrigo Ferreira <119365213+rodriaum@users.noreply.github.com>
2024-03-06 21:48:05 +0000

    Ficheiros iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,41 @@
+let score = 0
+let range = 0
+
+let started = false
+
+input.onButtonPressed(Button.A, function() {
+    if (!started) {
+        music.play(music.stringPlayable("C D E F G A B C5 ", 500), music.PlaybackMode.UntilDone)
+        basic.showNumber(score)
+        started = true
+    } else {
+        end()
+    }
+})
+
+input.onButtonPressed(Button.B, function () {
+    range++
+    basic.showNumber(range)
+})
+
+basic.forever(function () {
+
+    if (started && pins.digitalReadPin(DigitalPin.P1) == 1) {
+        score++
+        music.startMelody(music.builtInMelody(Melodies.JumpUp), MelodyOptions.Once)
+        basic.showNumber(score)
+
+        if (started && score >= range)
+            end()
+    }
+})
+
+function end() {
+    music.play(music.stringPlayable("C5 B A G F E D C ", 500), music.PlaybackMode.UntilDone)
+    basic.clearScreen()
+    basic.showString("WIN", 150)
+    basic.clearScreen()
+    started = false
+    score = 0
+    range = 0
+}
